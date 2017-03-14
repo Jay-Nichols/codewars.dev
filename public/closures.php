a9f4f88ef27b44580c47211c07ebda311c413c77
jay nichols <jay_nichols_3@hotmail.com>
2017-03-14 12:28:11 -0500

    php closure exercise on codewars

---
+++ public/closures.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$hello_world = function(){
+	return "Hello World";
+};
+
+$person_description = function($name, $age, $occupation){
+	return $name . " is " . $age . " years old and currently works as a(n) " . $occupation;
+}
